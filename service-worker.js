755b20c029bec800b4ea4759461f6185ff8b5c54
ErikEhresman-ST1987 <brainstorm60@gmail.com>
2026-09-12 16:19:43 -0700

    Refresh cache for backup and restore

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "personal-dashboard-v9";
+const CACHE_NAME = "personal-dashboard-v10";
 const APP_SHELL = ["./", "./index.html", "./bible-readings.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
