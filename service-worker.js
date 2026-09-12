dcac0dcb3b815350affdcf8c17e5f97de8b25a90
ErikEhresman-ST1987 <brainstorm60@gmail.com>
2026-09-12 16:01:09 -0700

    Refresh cache for Spiritual tab

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "personal-dashboard-v8";
+const CACHE_NAME = "personal-dashboard-v9";
 const APP_SHELL = ["./", "./index.html", "./bible-readings.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
