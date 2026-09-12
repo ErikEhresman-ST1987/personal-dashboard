1f0daeef8f882ca94cdccab41484cc331a844522
ErikEhresman-ST1987 <brainstorm60@gmail.com>
2026-09-12 16:51:33 -0700

    Refresh cache for active tab rendering

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "personal-dashboard-v11";
+const CACHE_NAME = "personal-dashboard-v12";
 const APP_SHELL = ["./", "./index.html", "./bible-readings.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
