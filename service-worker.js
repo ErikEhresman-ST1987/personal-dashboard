180bec450e7a6d62f7f306b99ee6c7e3cea6e321
ErikEhresman-ST1987 <brainstorm60@gmail.com>
2026-09-16 19:54:44 -0700

    Activate dashboard cache v13

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "personal-dashboard-v12";
-const APP_SHELL = ["./", "./index.html", "./bible-readings.js", "./manifest.webmanifest"];
+const CACHE_NAME = "personal-dashboard-v13";
+const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
